ad7768df5d716c6f42f25a919f17a3596bf726b9
shenhuanjie <shenhuanjie@live.cn>
2019-02-21 17:50:47 +0800

    提交：React语法

---
+++ src/list.jsx
@@ -0,0 +1,32 @@
+class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [1, 2, 3],
+            activeIndex: -1
+        };
+    }
+    activate(index) {
+        this.setState({
+            activeIndex: index
+        });
+    }
+    render() {
+        const { list, activeIndex } = this.state;
+        const lis = list.map(
+            (item, index) => {
+                const cls = index === activeIndex ? 'active' : '';
+                return (
+                    <li
+                        key={index}
+                        className={cls}
+                        onClick={() => this.activate(index)} >
+                    </li >
+                )
+            }
+        )
+        return (
+            <ul> {lis}</ul >
+        );
+    }
+}
